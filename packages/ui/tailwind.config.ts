2be500c53055885ddd02c3e7e6b9ee105b0c24ae
Yoonjae Choi <dbswo9795@gmail.com>
2024-08-03 13:30:54 +0900

    chore: init ui packages

---
+++ packages/ui/tailwind.config.ts
@@ -0,0 +1,12 @@
+import type { Config } from 'tailwindcss';
+const config: Config = {
+	content: [
+		'../../apps/storybook/**/*.{js,ts,jsx,tsx,mdx}',
+		'./.stories/**/*.{js,ts,jsx,tsx,mdx}',
+		'./src/**/*.{js,ts,jsx,tsx,mdx}',
+	],
+	theme: {
+		extend: {},
+	},
+};
+export default config;
