eb49079437ec561a1e7663e8d5938d1c590e2465
Sam Choi <dbswo9795@gmail.com>
2024-11-22 23:14:42 +0900

    feat: 로컬 스토리지 훅 구현

--- packages/hooks/src/index.ts
+++ packages/hooks/src/index.ts
@@ -1,5 +1,6 @@
 export { useFirstRender } from './use-first-render';
 export { usePromise } from './use-promise';
+export { useLocalStorage } from './use-local-storage';
 
 export type { Callback } from './use-first-render';
 export type {
