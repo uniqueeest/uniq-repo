3bd7233c6ff178ba75adf10ccdb7ff76977e0dc9
Sam Choi <dbswo9795@gmail.com>
2025-01-06 22:59:23 +0900

    remove: 불필요 패키지 컴포넌트 제거

--- packages/ui/src/index.ts
+++
@@ -1,7 +0,0 @@
-export { Button } from './Button';
-export { Input } from './Input';
-export { Spacing } from './Spacing';
-export { View } from './View';
-
-export type { ViewType } from './View';
-export type { ButtonType } from './Button';
