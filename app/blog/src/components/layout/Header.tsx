9e161a51b71c52a20140bbd82cfab0381a70bd26
Sam Choi <dbswo9795@gmail.com>
2025-01-07 13:04:56 +0900

    feat: 헤더 구현

--- app/blog/src/components/layout/Header.tsx
+++ app/blog/src/components/layout/Header.tsx
@@ -1,3 +1,16 @@
+import Image from 'next/image';
+
+import { cn } from '@lib';
+
 export const Header = () => {
-  return <header>헤더입니다.</header>;
+  return (
+    <header className={cn('lg:py-5', 'bg-gray-4 border-b border-b-gray-8-')}>
+      <div
+        className={cn('flex items-center gap-5', 'lg:mx-auto lg:w-[1280px]')}
+      >
+        <Image src="/uniqueeest.png" alt="logo" width={40} height={40} />
+        <h1 className="text-2xl font-medium">uniqueeest</h1>
+      </div>
+    </header>
+  );
 };
