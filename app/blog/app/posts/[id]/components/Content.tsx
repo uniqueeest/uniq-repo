a9dc7d09e09f8a429478e07f6fc8b4e680622483
Sam Choi <dbswo9795@gmail.com>
2025-01-07 18:54:46 +0900

    fix: 콘텐츠 컴포넌트 수정

---
+++ app/blog/app/posts/[id]/components/Content.tsx
@@ -0,0 +1,23 @@
+import { cn } from '@lib';
+
+import markdownStyles from './markdown-styles.module.css';
+
+interface ContentProps {
+  content: string;
+}
+
+export const Content = ({ content }: ContentProps) => {
+  return (
+    <section
+      className={cn(
+        'flex items-center gap-5',
+        'lg:pb-10 lg:mx-auto lg:w-[1280px]',
+      )}
+    >
+      <article
+        className={markdownStyles['markdown']}
+        dangerouslySetInnerHTML={{ __html: content }}
+      />
+    </section>
+  );
+};
