ed397b1f16d80a2c58278a35ef1475ba7a0bdfc6
Yoonjae Choi <dbswo9795@gmail.com>
2025-01-07 13:06:08 +0900

    feat: 헤더 구현 (#11)
    * chore: 파비콘, 폰트 수정
    
    * feat: 헤더 구현

--- app/blog/app/layout.tsx
+++ app/blog/app/layout.tsx
@@ -1,9 +1,22 @@
 import type { Metadata } from 'next';
+import localFont from 'next/font/local';
 
 import './globals.css';
+import { Header } from '@components/layout';
+
+const pretendard = localFont({
+  src: './fonts/PretendardVariable.woff2',
+  variable: '--font-pretendard',
+  weight: '400 500 700',
+  preload: true,
+  display: 'swap',
+});
 
 export const metadata: Metadata = {
   title: 'uniqueeests',
+  icons: {
+    icon: '/uniqueeest.png',
+  },
 };
 
 export default function RootLayout({
@@ -12,8 +25,11 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body>{children}</body>
+    <html lang="ko">
+      <body className={pretendard.className}>
+        <Header />
+        {children}
+      </body>
     </html>
   );
 }
