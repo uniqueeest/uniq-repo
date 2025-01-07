d9869ddc500a6553e3120ac5e97f72cc3b021b58
Sam Choi <dbswo9795@gmail.com>
2025-01-07 16:23:25 +0900

    feat: 메인 페이지 기본 마크업

--- app/blog/app/page.tsx
+++ app/blog/app/page.tsx
@@ -1,3 +1,22 @@
+import Link from 'next/link';
+
+import { getAllPosts } from '@lib';
+
 export default function Home() {
-  return <div></div>;
+  const allPosts = getAllPosts();
+
+  return (
+    <section>
+      <ul>
+        {allPosts.map((post) => (
+          <li key={post.slug}>
+            <Link href={`/posts/${post.slug}`}>
+              <h3>{post.title}</h3>
+              <p>{post.author.name}</p>
+            </Link>
+          </li>
+        ))}
+      </ul>
+    </section>
+  );
 }
