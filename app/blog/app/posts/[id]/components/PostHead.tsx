c2645d95970fa6493f724d39b63ab383f41eecb3
Sam Choi <dbswo9795@gmail.com>
2025-01-12 20:40:35 +0900

    rename: 경로 수정 및 스타일 추가

--- app/blog/app/posts/[id]/components/PostHead.tsx
+++ app/blog/app/posts/[id]/components/PostHead.tsx
@@ -1,7 +1,8 @@
 import dayjs from 'dayjs';
 import 'dayjs/locale/ko';
 
-import { cn } from '@lib';
+import { cn } from '@utils';
+import { NICKNAME } from '@constants/nickname';
 
 interface PostHeadProps {
   title: string;
@@ -13,7 +14,7 @@ export const PostHead = ({ title, date }: PostHeadProps) => {
     <section
       className={cn(
         'flex flex-col gap-4',
-        'lg:pt-5 lg:pb-10 lg:mx-auto lg:w-[1280px]',
+        'lg:pt-5 lg:pb-10 lg:center-1280',
         'border-b border-b-gray-8',
       )}
     >
@@ -21,7 +22,7 @@ export const PostHead = ({ title, date }: PostHeadProps) => {
         <p className="text-sm text-gray-10">
           {dayjs(date).format('YYYY년 MM월 DD일')}
         </p>
-        <p className="text-xl text-blue-12 font-semibold">uniqueeest</p>
+        <p className="text-xl text-blue-12 font-semibold">{NICKNAME}</p>
       </div>
       <h2 className="font-bold text-gray-12 text-[60px]">{title}</h2>
     </section>
--- app/blog/src/components/layout/Header.tsx
+++ app/blog/src/components/layout/Header.tsx
@@ -1,15 +1,19 @@
+import Link from 'next/link';
 import Image from 'next/image';
 
-import { cn } from '@lib';
+import { cn } from '@utils';
 
 export const Header = () => {
   return (
     <header className={cn('lg:py-5', 'bg-gray-4 border-b border-b-gray-7')}>
-      <div
-        className={cn('flex items-center gap-5', 'lg:mx-auto lg:w-[1280px]')}
-      >
-        <Image src="/uniqueeest.png" alt="logo" width={40} height={40} />
-        <h1 className="text-2xl font-medium">uniqueeest</h1>
+      <div className="lg:center-1280">
+        <Link
+          href="/"
+          className={cn('flex items-center gap-5', 'cursor-pointer')}
+        >
+          <Image src="/uniqueeest.png" alt="logo" width={40} height={40} />
+          <h1 className="text-2xl font-medium">uniqueeest</h1>
+        </Link>
       </div>
     </header>
   );
