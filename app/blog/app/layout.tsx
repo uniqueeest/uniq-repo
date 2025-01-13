1859cbaadc05230382c988828fbed31e0b297869
Sam Choi <dbswo9795@gmail.com>
2025-01-13 23:08:57 +0900

    feat: 푸터 구현

--- app/blog/app/layout.tsx
+++ app/blog/app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from 'next';
 import localFont from 'next/font/local';
 
 import './globals.css';
-import { Header } from '@components/layout';
+import { Footer, Header } from '@components/layout';
 
 const pretendard = localFont({
   src: './fonts/PretendardVariable.woff2',
@@ -29,6 +29,7 @@ export default function RootLayout({
       <body className={pretendard.className}>
         <Header />
         {children}
+        <Footer />
       </body>
     </html>
   );
