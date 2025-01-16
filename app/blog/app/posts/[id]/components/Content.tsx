451cd4d040810496099ee72b7614efd4ff5f051b
Yoonjae Choi <dbswo9795@gmail.com>
2025-01-16 18:12:27 +0900

    feat: 메타 데이터 추가 (#22)
    * chore: alias 추가
    
    * style: 스타일 수정
    
    * fix: 메타 데이터 추가
    
    * style: 스타일 수정

--- app/blog/app/posts/[id]/components/Content.tsx
+++ app/blog/app/posts/[id]/components/Content.tsx
@@ -9,10 +9,7 @@ interface ContentProps {
 export const Content = ({ content }: ContentProps) => {
   return (
     <section
-      className={cn(
-        'flex items-center gap-5',
-        'py-5 px-3 lg:py-10 lg:center-1280',
-      )}
+      className={cn('flex items-center gap-5', 'py-5 lg:py-10 lg:center-1280')}
     >
       <article
         className={markdownStyles['markdown']}
---
+++ app/blog/src/constants/url.ts
@@ -0,0 +1,1 @@
+export const BLOG_URL = 'https://uniqueeest.site';
