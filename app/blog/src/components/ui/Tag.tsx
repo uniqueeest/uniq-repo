7352d68751d11bf7a7a0f3d34f8df4da69880735
Yoonjae Choi <dbswo9795@gmail.com>
2025-01-22 11:58:26 +0900

    feat: 태그 컴포넌트 구현

---
+++ app/blog/src/components/ui/Tag.tsx
@@ -0,0 +1,30 @@
+import { cn } from '@utils';
+
+interface TagProps {
+  tag: string;
+  color?: 'red' | 'green' | 'blue' | 'yellow' | 'gray';
+  className?: string;
+}
+
+const COLORS = {
+  red: 'bg-red-7',
+  green: 'bg-green-7',
+  blue: ' bg-blue-7',
+  yellow: 'bg-yellow-7',
+  gray: 'bg-gray-7',
+};
+
+export const Tag = ({ tag, color = 'blue', className }: TagProps) => {
+  return (
+    <div
+      className={cn(
+        'py-1 px-2 w-fit h-fit',
+        'text-xs lg:text-sm text-white rounded-[10px]',
+        color && COLORS[color],
+        className,
+      )}
+    >
+      {tag}
+    </div>
+  );
+};
