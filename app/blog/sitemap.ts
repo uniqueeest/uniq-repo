f342855c736fc24f9add305f4b2e280b081f31d0
Yoonjae Choi <dbswo9795@gmail.com>
2025-01-25 11:02:38 +0900

    chore: 사이트맵 추가 (#28)
    * chore: sitemap, robot 추가
    
    * chore: google search console 연결
    
    * remove: 미사용 변수 제거

---
+++ app/blog/sitemap.ts
@@ -0,0 +1,20 @@
+import type { MetadataRoute } from 'next';
+
+import { BLOG_URL } from '@constants/url';
+import { getAllPosts } from '@lib';
+
+export default function sitemap(): MetadataRoute.Sitemap {
+  const posts = getAllPosts();
+
+  const postUrl = posts.map((post) => ({
+    url: `${BLOG_URL}/posts/${post.slug}`,
+    lastModified: new Date(),
+  }));
+
+  const rootUrl = {
+    url: BLOG_URL,
+    lastModified: new Date(),
+  };
+
+  return [rootUrl, ...postUrl];
+}
