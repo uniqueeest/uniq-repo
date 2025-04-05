0362c2a6fc0845dc2f084b1f33c56d8977e39e9f
Yoonjae Choi <dbswo9795@gmail.com>
2025-04-05 16:09:09 +0900

    style: 기본 스타일 구현

---
+++ app/pildam-zip/tailwind.config.ts
@@ -0,0 +1,27 @@
+import type { Config } from 'tailwindcss';
+import { STYLE } from '@uniqueeest/token';
+
+export default {
+  content: [
+    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+    './app/**/*.{js,ts,jsx,tsx,mdx}',
+  ],
+  theme: {
+    extend: {
+      colors: {
+        background: STYLE.colors.gray[2],
+        gray: STYLE.colors.gray,
+        blue: STYLE.colors.blue,
+        green: STYLE.colors.green,
+        red: STYLE.colors.red,
+        white: STYLE.colors.white,
+        black: STYLE.colors.black,
+      },
+      borderRadius: STYLE.borderRadius,
+      fontSize: STYLE.fontSize,
+      fontWeight: STYLE.fontWeight,
+      spacing: STYLE.spacing,
+    },
+  },
+  plugins: [],
+} satisfies Config;
