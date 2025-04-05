4a8559b545d0a922f3881bd2074466df13030362
Yoonjae Choi <dbswo9795@gmail.com>
2025-04-06 00:38:44 +0900

    feat: 페이지 추가

--- app/pildam-zip/app/page.tsx
+++ app/pildam-zip/app/page.tsx
@@ -1,9 +1,21 @@
+import { cn } from '@uniqueeest/utils';
+
+import { PostCard } from '@/entities/post';
+
 export default function Home() {
   return (
-    <div className="center-1020">
-      <div className="flex flex-col items-center justify-center h-screen">
-        <h1 className="text-4xl font-bold text-blue-5">안녕하세요.</h1>
-      </div>
-    </div>
+    <section className="center-700">
+      <article className={cn('grid grid-cols-1 gap-10', 'md:grid-cols-2')}>
+        {Array.from({ length: 10 }).map((_, index) => (
+          <PostCard
+            key={index}
+            title="안녕"
+            description="안녕"
+            date="2021-01-01"
+            slug={`${index}`}
+          />
+        ))}
+      </article>
+    </section>
   );
 }
