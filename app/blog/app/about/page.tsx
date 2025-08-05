37b64348d4a436fef8f7efd3a7b96a0a70c75522
Yoonjae Choi <dbswo9795@gmail.com>
2025-08-06 00:07:40 +0900

    chore: 메타데이터 추가

--- app/blog/app/about/page.tsx
+++ app/blog/app/about/page.tsx
@@ -1,5 +1,36 @@
+import { Metadata } from 'next';
 import { Navigation } from '@shared/ui/Navigation';
 import { cn } from '@uniqueeest/utils';
+import { BLOG_URL } from '@shared/constants/url';
+import { NICKNAME } from '@shared/constants/nickname';
+
+export const metadata: Metadata = {
+  metadataBase: new URL(BLOG_URL),
+  title: 'uniqueeest',
+  description:
+    '프로덕트의 방향성을 함께 고민하고 비즈니스 가치 창출에 기여하는 개발자를 지향합니다.',
+  openGraph: {
+    title: 'uniqueeest',
+    url: `${BLOG_URL}/about`,
+    siteName: NICKNAME,
+    type: 'website',
+    images: [
+      {
+        url: '/meta-image.png',
+        width: 1200,
+        height: 630,
+        alt: 'About uniqueeest',
+      },
+    ],
+  },
+  twitter: {
+    card: 'summary_large_image',
+    title: 'uniqueeest',
+    description:
+      '프로덕트의 방향성을 함께 고민하고 비즈니스 가치 창출에 기여하는 개발자를 지향합니다.',
+    images: ['/meta-image.png'],
+  },
+};
 
 export default function About() {
   return (
