e12615b994493e4e9a02aeee1a3dc51ded5eebfe
Yoonjae Choi <dbswo9795@gmail.com>
2025-10-05 16:01:39 +0900

    fix: index에 추가

--- packages/hooks/src/index.ts
+++ packages/hooks/src/index.ts
@@ -1,7 +1,9 @@
+export { useBeforeUnload } from './use-before-unload';
+export { useBlocker } from './use-blocker';
 export { useFirstRender } from './use-first-render';
-export { usePromise } from './use-promise';
 export { useLocalStorage } from './use-local-storage';
 export { useNestedState } from './use-nested-state';
+export { usePromise } from './use-promise';
 export { useScrollDirection } from './use-scroll-direction';
 
 export type { Callback } from './use-first-render';
